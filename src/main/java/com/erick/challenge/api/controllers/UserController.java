803a7e5a90651b2a9d8cdf6942975a2bbef6c4fe
Erick Silva <efsgarbage@gmail.com>
2024-06-24 15:53:36 -0300

    CHALLENGE-S2-3: creation of the route /api/users/{id} Search for a user by id

--- src/main/java/com/erick/challenge/api/controllers/UserController.java
+++ src/main/java/com/erick/challenge/api/controllers/UserController.java
@@ -2,10 +2,12 @@ package com.erick.challenge.api.controllers;
 
 import java.net.URI;
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,5 +39,11 @@ public class UserController {
 		List<User> list = userService.findAll();
 		List<UserDTO> listDTO = list.stream().map(obj -> new UserDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable UUID id) {
+		User obj = userService.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
 }
