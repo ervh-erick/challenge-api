1d0b26cc563cf7cfd1b6a702976ffdd77b89d9c3
Erick Silva <efs9@dmz.org.br>
2024-06-30 20:59:43 -0300

    CHALLENGE-S4-4:  /api/cars/{id} Buscar um carro do usuário logado pelo id

--- src/main/java/com/erick/challenge/api/controllers/CarController.java
+++ src/main/java/com/erick/challenge/api/controllers/CarController.java
@@ -2,10 +2,12 @@ package com.erick.challenge.api.controllers;
 
 import java.net.URI;
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.erick.challenge.api.domain.Car;
+import com.erick.challenge.api.domain.User;
 import com.erick.challenge.api.domain.dto.CarDTO;
+import com.erick.challenge.api.domain.dto.UserDTO;
 import com.erick.challenge.api.services.CarService;
 
 import jakarta.validation.Valid;
@@ -39,6 +43,12 @@ public class CarController {
 		List<CarDTO> listDTO = list.stream().map(obj -> new CarDTO(obj)).collect(Collectors.toList());
 
 		return ResponseEntity.ok().body(listDTO);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<CarDTO> findById(@PathVariable UUID id) {
+		Car obj = carService.findById(id);
+		return ResponseEntity.ok().body(new CarDTO(obj));
 	}
 
 }
